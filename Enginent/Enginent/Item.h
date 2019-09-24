f0667feb13b3f33a3e38dc088e3aa51404e9c565
Flairmin <wapee_o@hotmail.co.th>
2019-09-24 13:37:42 +0700

    test push

---
+++ Enginent/Enginent/Item.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "InteractObj.h"
+
+class Item : public InteractableObj {
+public:
+	
+	/*public Sprite sprite;
+	public SpriteRenderer spriteRenderer;
+
+	void Start()
+	{
+		spriteRenderer.sprite = sprite;
+		SetType(IneractTypeList.ITEM);
+	}
+
+	public override void action()
+	{
+		GameController.instance.player.inventory.AddItem(gameObject);
+	}*/
+};
