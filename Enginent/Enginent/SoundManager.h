23fe7eb8a96ba7cf0372775b7868884a3ebc3ab0
wapee <wapee_o@sci.tu.ac.th>
2019-09-29 19:02:29 +0700

    Added some methods to soundManager: - pause() --> pause all sound - unpause() --> unpause all sound - upVolume() --> increase volume by a fixed amuont - downVolume() --> decrease volume by a fixed amuont - destructor() --> delete the soundEngine properly

--- Enginent/Enginent/SoundManager.h
+++ Enginent/Enginent/SoundManager.h
@@ -10,5 +10,10 @@ public:
 	SoundManager();
 	void playBg(const char* filename);
 	void playSFX(const char* filename);
+	void pause();
+	void unPause();
 	void stopBg();
+	void upVolume();
+	void downVolume();
+	~SoundManager();
 };
