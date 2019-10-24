28fe4595547d2cca0177e7beca066f0072448220
wapee <wapee_o@sci.tu.ac.th>
2019-10-24 10:25:31 +0700

    no message

--- Enginent/Enginent/LevelGenerator.cpp
+++ Enginent/Enginent/LevelGenerator.cpp
@@ -1,5 +1,5 @@
 #include "LevelGenerator.h"
-
+#include "Game.h"
 
 void RoomGenerator::GenerateRoom(std::string filename) {
 	LoadFile(filename);
@@ -9,6 +9,16 @@ void RoomGenerator::GenerateRoom(std::string filename) {
 		for (pugi::xml_node childChild = child.first_child(); childChild; childChild = child.next_sibling()) {
 			// get attributes
 			// createObject
+			int type = childChild.find_attribute("type").as_int();
+			std::string texture = childChild.find_attribute("texture").as_string();
+			int sizeX = childChild.find_attribute("sizeX").as_int();
+			int sizeY = childChild.find_attribute("sizeY").as_int();
+			glm::vec3 pos = glm::vec3(childChild.find_attribute("posX").as_float(),
+									  childChild.find_attribute("posY").as_float(),
+									  1.0f);
+			std::string dialoguePath = childChild.find_attribute("dialogue").as_string();
+
+			Game::GetInstance()->createObject(type, texture, sizeX, sizeY, pos, dialoguePath);
 		}
 	}
 }
