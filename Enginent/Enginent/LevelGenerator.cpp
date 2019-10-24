8ac84480f3a208836c9482a7989555f09d4a6a82
wapee <wapee_o@sci.tu.ac.th>
2019-10-24 10:11:31 +0700

    progress

--- Enginent/Enginent/LevelGenerator.cpp
+++ Enginent/Enginent/LevelGenerator.cpp
@@ -3,5 +3,12 @@
 
 void RoomGenerator::GenerateRoom(std::string filename) {
 	LoadFile(filename);
-	// do something with doc using createObject method from game
+	pugi::xml_node node = doc.first_child().first_child();	// get the first node of the root
+	for (pugi::xml_node child = node.first_child(); child; child = child.next_sibling()) {
+		// loop through headers
+		for (pugi::xml_node childChild = child.first_child(); childChild; childChild = child.next_sibling()) {
+			// get attributes
+			// createObject
+		}
+	}
 }
