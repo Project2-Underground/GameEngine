a1e789a78c92b49531f178b7f6b38f442420c8d3
wapee <wapee_o@sci.tu.ac.th>
2019-10-25 09:32:15 +0700

    fix some format in example xml file

--- Enginent/Enginent/LevelGenerator.cpp
+++ Enginent/Enginent/LevelGenerator.cpp
@@ -2,14 +2,15 @@
 #include "Game.h"
 
 void RoomGenerator::GenerateRoom(std::string filename) {
+	/* generate room base on the xml doc */
 	LoadFile(filename);
 	pugi::xml_node node = doc.first_child().first_child();	// get the first node of the root
 	for (pugi::xml_node child = node.first_child(); child; child = child.next_sibling()) {
 		// loop through headers
+		int type = child.find_attribute("type").as_int();
 		for (pugi::xml_node childChild = child.first_child(); childChild; childChild = child.next_sibling()) {
 			// get attributes
 			// createObject
-			int type = childChild.find_attribute("type").as_int();
 			std::string texture = childChild.find_attribute("texture").as_string();
 			int sizeX = childChild.find_attribute("sizeX").as_int();
 			int sizeY = childChild.find_attribute("sizeY").as_int();
