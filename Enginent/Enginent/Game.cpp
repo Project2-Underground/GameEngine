2bb9fa4715cf18189cf52dc94abc0a3b56099197
Natnaree <fafuang@gmail.com>
2019-11-22 14:14:03 +0700

    no message

--- Enginent/Enginent/Game.cpp
+++ Enginent/Enginent/Game.cpp
@@ -7,6 +7,8 @@
 #include "TextObject.h"
 #include "InteractObj.h"
 #include "LevelGenerator.h"
+#include "Cursor.h"
+#include "Button.h"
 
 Game* Game::instance = nullptr;
 
@@ -25,6 +27,19 @@ GLRenderer * Game::GetRenderer()
 	return this->renderer;
 }
 
+int Game::checkPointObject(glm::vec3 pos)
+{
+	for (int i = objects.size() - 1; i >= 0; i--)
+	{
+		if (dynamic_cast<InteractableObj*>(objects[i]) && dynamic_cast<InteractableObj*>(objects[i])->checkPointing(pos.x, pos.y))
+		{
+			return dynamic_cast<InteractableObj*>(objects[i])->getType();
+		}
+	}
+
+	return 0;
+}
+
 void Game::rightClick(int x, int y)
 {
 	float realX, realY;
@@ -38,14 +53,33 @@ void Game::leftClick(int x, int y)
 	float realX, realY;
 	realX = -(winWidth * 0.5) + x;
 	realY = -(winHeight * 0.5) + (winHeight - y);
-	for (int i = 0; i < colliders.size(); i++)
+	for (int i = 0; i < objects.size(); i++)
 	{
-		if (colliders[i]->isClicked(realX, realY)) {
-			cout << "collide\n";
-			printf("%d\n", i);
+		if (dynamic_cast<InteractableObj*>(objects[i]))
+		{
+			((InteractableObj*)objects[i])->checkCollider(realX, realY);
 		}
 	}
 
+	for (int j = 0; j < UI.size(); j++)
+	{
+		if (dynamic_cast<Button*>(UI[j]))
+		{
+			((Button*)UI[j])->checkCollider(realX, realY);
+		}
+	}
+}
+
+void Game::updateMouseState(int x, int y)
+{
+	glm::vec3 realPos = Game::GetInstance()->findRealPos(x, y);
+	for (int i = 0; i < UI.size(); i++)
+	{
+		if (dynamic_cast<Button*>(UI[i]))
+		{
+			((Button*)UI[i])->updateButton(realPos.x, realPos.y);
+		}
+	}
 }
 
 void Game::HandleKey(char ch)
@@ -53,10 +87,10 @@ void Game::HandleKey(char ch)
 	if (this->objects.size() > 0) {
 		DrawableObject *obj = this->objects.at(2);
 		switch (ch) {
-		case 'u': player->setDialogue("Fly"); break;
-		case 'd': break;
-		case 'l': break;
-		case 'r': break;
+		case 'u': player->setDialogue("Up"); break;
+		case 'd': player->setDialogue("Down"); break;
+		case 'l': player->setDialogue("Left"); break;
+		case 'r': player->setDialogue("Right"); break;
 		}
 	}
 }
@@ -69,6 +103,8 @@ void Game::Init(int width, int height)
 	renderer->InitGL("Shader/vertext.shd", "Shader/fragment.shd");
 	renderer->SetOrthoProjection(-width * 0.5f, width * 0.5f, -height * 0.5f, height * 0.5f);
 	renderer->SetClearColor(1.0f, 1.0f, 200.0f / 255);
+
+	SDL_ShowCursor(SDL_DISABLE);
 
 	SquareMeshVbo * square = new SquareMeshVbo();
 	square->LoadData();
@@ -99,27 +135,43 @@ void Game::Init(int width, int height)
 	//
 	player = new Player();
 	player->SetTexture("Texture/Character/Elias.png");
-	player->SetSize(150.0f, -340.0f);
+	player->SetSize(230.0f, -350.0f);
 	player->SetPosition(glm::vec3(0.0f, -50.0f, 1.0f));
+	player->setDisplay(false);
 	objects.push_back(player);
-
-	objects.push_back(player->setDialogue(" "));
 
 	Collider *col = new Collider(player);
 	colliders.push_back(col);
 	player->SetCollder(col);
+
+	player->anim->Play("Move", true);
+
+	objects.push_back(player->createDialogueText());
+
+	//Item* item1 = new Item("temp");
+	//item1->SetTexture("Texture/EliasRoom/cloth.png");
+
+	//player->inventory->addItem(item1);
+	//player->inventory->addItem(item1);
+	//player->inventory->addItem(item1);
+	//player->inventory->addItem(item1);
+	//player->inventory->addItem(item1);
+
 
 
 }
 
 void Game::Update()
 {
-	player->Move();
+	player->Update();
+	player->anim->Update();
+	cursorGame->updateCursor();
 }
 
 void Game::Render()
 {
 	this->GetRenderer()->Render(this->objects);
+	this->GetRenderer()->Render(this->UI);
 }
 
 Game::Game()
@@ -130,49 +182,70 @@ Game::Game()
 	renderer = nullptr;
 }
 
-void Game::createObject(int type, std::string texture, int sizeX, int sizeY, glm::vec3 pos, std::string dialogue)
+void Game::createObject(int type, std::string texture, int sizeX, int sizeY, glm::vec3 pos, IneractTypeList objType, vector<std::string>* dialogue)
 {
 	ImageObject *tmp = nullptr;
 	switch (type)
 	{
-		case IMAGE_OBJ:
+	case IMAGE_OBJ:
+	{
+		tmp = new ImageObject();
+		break;
+	}
+	case INTERACT_OBJ:
+	{
+		if (dialogue != nullptr)
 		{
-			tmp = new ImageObject();
-			break;
-		}
-		case INTERACT_OBJ:
-		{
-			if(dialogue != "")
-			{
-				tmp = new InteractableObj(dialogue);
-				Collider *col2 = new Collider(tmp);
-				colliders.push_back(col2);
-				((InteractableObj*)tmp)->SetCollder(col2);
+			tmp = new InteractableObj(objType, dialogue);
 
-			}
-			else
-			{
-				tmp = new InteractableObj();
-				Collider *col2 = new Collider(tmp);
-				colliders.push_back(col2);
-				((InteractableObj*)tmp)->SetCollder(col2);
-			}
-			break;
 		}
-		case PORTOL:
+		else
 		{
-			break;
+			tmp = new InteractableObj();
 		}
-		case NPC:
-		{
-			break;
-		}
+		break;
+	}
+	case PORTOL:
+	{
+		break;
+	}
+	case NPC:
+	{
+		break;
+	}
 	}
 
 	tmp->SetTexture(texture);
 	tmp->SetSize(sizeX, sizeY);
 	tmp->SetPosition(pos);
+	if (type != IMAGE_OBJ)
+	{
+		Collider *col2 = new Collider(tmp);
+		colliders.push_back(col2);
+		((InteractableObj*)tmp)->SetCollder(col2);
+	}
 
 	objects.push_back(tmp);
+}
+
+void Game::AddObject(DrawableObject* obj) {
+	objects.push_back(obj);
+}
+
+void Game::AddUI(UIObject* obj) {
+	UI.push_back(obj);
+}
+
+Game::~Game()
+{
+
+}
+
+glm::vec3 Game::findRealPos(int x, int y)
+{
+	float realX, realY;
+	realX = -(winWidth * 0.5) + x;
+	realY = -(winHeight * 0.5) + (winHeight - y);
+	return glm::vec3(realX, realY, 1);
 }
 
--- Enginent/Enginent/Game.h
+++ Enginent/Enginent/Game.h
@@ -5,6 +5,8 @@
 #include "DrawableObject.h"
 #include "GLRenderer.h"
 #include "Collider.h"
+#include "Cursor.h"
+#include "SoundManager.h"
 
 using namespace std;
 class Game
@@ -12,10 +14,12 @@ class Game
 	static Game* instance;
 	int winWidth, winHeight;
 	vector<DrawableObject*> objects;
+	vector<UIObject*> UI;
 	vector<Collider*> colliders;
 	GLRenderer *renderer;
 	Player * player;
 	Game();
+	CursorUI* cursorGame;
 public:
 	static Game* GetInstance();
 	GLRenderer * GetRenderer();
@@ -25,8 +29,14 @@ public:
 	void Init(int width, int height);
 	void Update();
 	void Render();
-	void createObject(int type, std::string texture, int sizeX, int sizeY, glm::vec3 pos, std::string dialogue);
-
+	void createObject(int type, std::string texture, int sizeX, int sizeY, glm::vec3 pos, IneractTypeList objType, std::string dialoguePath);
+	void AddObject(DrawableObject* obj);
+	void AddUI(UIObject* obj);
+	void updateMouseState(int, int);
+	Player* getPlayer() { return player; }
+	glm::vec3 findRealPos(int x, int y);
+	int checkPointObject(glm::vec3 pos);
+	~Game();
 
 };
 
