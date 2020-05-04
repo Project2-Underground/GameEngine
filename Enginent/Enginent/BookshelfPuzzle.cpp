aba7f9e8bb94d95b5b4f6c5bc2a679e5d626a0ab
Natnaree <fafuang@gmail.com>
2020-05-04 16:12:39 +0700

    puzzle 5 finish (no reset)

--- Enginent/Enginent/BookshelfPuzzle.cpp
+++ Enginent/Enginent/BookshelfPuzzle.cpp
@@ -339,3 +339,329 @@ BookshelfPuzzle::~BookshelfPuzzle()
 void BookshelfPuzzle::CompletePuzzle() {
 	puzzle->CompletePuzzle();
 }
+
+Book2::Book2(int id, std::string texture, float sizeX, float sizeY, int posX, int posY, int paperID)
+{
+	this->paper_id = paperID;
+	this->paper = nullptr;
+	this->id = id;
+	this->SetTexture(texture);
+	this->SetSize(sizeX, sizeY);
+	this->SetPosition(glm::vec3(posX, posY, 0));
+	this->col = new Collider(this);
+}
+
+Book2::~Book2()
+{
+
+}
+
+bool Book2::checkPaper()
+{
+	if (paper_id == paper->ID)
+	{
+		return true;
+	}
+	return false;
+}
+
+Bookshelf2::Bookshelf2(std::string texture, int posX, int posY, int sizeX, int sizeY)
+{
+	this->texture = new UIObject();
+	this->texture->SetTexture(texture);
+	this->texture->SetPosition(glm::vec3(posX, posY, 1));
+	this->texture->SetSize((float)sizeX, (float)sizeY);
+}
+
+void Bookshelf2::Init(std::vector<Book*> book1, std::vector<Book2*> book, std::vector<Space*> space, std::vector<UIObject*> img, std::vector<Paper*> paper)
+{
+	this->books_2 = book;
+	this->books = book1;
+	this->log = space;
+	this->images = img;
+	this->papers = paper;
+}
+
+void Bookshelf2::Render()
+{
+	for (int i = 0; i < images.size(); i++)
+	{
+		Game::GetInstance()->GetRenderer()->Render(images[i]);
+	}
+	for (int i = 0; i < log.size(); i++)
+	{
+		Game::GetInstance()->GetRenderer()->Render(log[i]->colTemp);
+	}
+	for (int i = 0; i < books.size(); i++)
+	{
+		Game::GetInstance()->GetRenderer()->Render(books[i]);
+	}
+	for (int i = 0; i < books_2.size(); i++)
+	{
+		Game::GetInstance()->GetRenderer()->Render(books_2[i]);
+	}
+	for (int i = 0; i < papers.size(); i++)
+	{
+		Game::GetInstance()->GetRenderer()->Render(papers[i]);
+	}
+}
+
+void Bookshelf2::Update()
+{
+	glm::vec3 screen = MouseInput::GetInstance()->GetMouseScreenPosition();
+	if (select != nullptr)
+	{
+		select->SetPosition(screen);
+	}
+	if (select_p != nullptr)
+	{
+		select_p->SetPosition(screen);
+	}
+	pass = true;
+	for (int i = 0; i < log.size(); i++)
+	{
+		if (log[i]->book == nullptr) {
+			pass = false;
+			break;
+		}
+		else if (log[i]->id != log[i]->book->GetId())
+		{
+			pass = false;
+			break;
+		}
+	}
+	for (int j = 0; j < books_2.size(); j++)
+	{
+		if (books_2[j]->paper == nullptr) {
+			pass = false;
+			break;
+		}
+		else if (books_2[j]->GetPaperid() != books_2[j]->paper->ID)
+		{
+			pass = false;
+			break;
+		}
+	}
+	if (pass && !doneAction) {
+		ActionAfterPuzzle();
+		doneAction = true;
+	}
+}
+
+void Bookshelf2::Reset()
+{
+
+}
+
+void Bookshelf2::LeftClick(glm::vec3 screen, glm::vec3 world)
+{
+	if (!pass) {
+		if (select == nullptr && select_p == nullptr)
+		{
+			for (int i = 0; i < books.size(); i++)
+			{
+				if (books[i]->col->isClicked(screen.x, screen.y))
+				{
+					select = books[i];
+				}
+			}
+			for (int j = 0; j < log.size(); j++)
+			{
+				if (log[j]->collider->isClicked(screen.x, screen.y))
+				{
+					select = log[j]->book;
+					log[j]->book = nullptr;
+				}
+			}
+		}
+		else
+		{
+			for (int j = 0; j < log.size(); j++)
+			{
+				if (log[j]->collider->isClicked(screen.x, screen.y))
+				{
+					select_s = log[j];
+				}
+			}
+		}
+		if (select_p == nullptr && select == nullptr)
+		{
+			for (int j = 0; j < papers.size(); j++)
+			{
+				if (papers[j]->col->isClicked(screen.x, screen.y) && !papers[j]->paste)
+				{
+					select_p = papers[j];
+				}
+			}
+		}
+	}
+}
+
+void Bookshelf2::LeftRelease(glm::vec3 screen, glm::vec3 world)
+{
+	if (select != nullptr && select_s != nullptr) {
+		if (select_s->collider->isClicked(screen.x, screen.y))
+		{
+			Book* tmp = select_s->book;
+			select_s->book = select;
+			select->SetPosition(select_s->colTemp->getPos());
+			select->col->setNewPos(select_s->colTemp->getPos());
+			select = tmp;
+			select_s = nullptr;
+		}
+	}
+	if (select_p)
+	{
+		bool placed = false;
+		for (int i = 0; i < books_2.size(); i++)
+		{
+			if (books_2[i]->col->isClicked(screen.x, screen.y) && books_2[i]->paper == nullptr)
+			{
+				select_p->paste = true;
+				select_p->SetTexture(select_p->book_paperTexture);
+				select_p->SetPosition(books_2[i]->getPos());
+				select_p->col->setNewPos(books_2[i]->getPos());
+				select_p->SetSize(46, -124);
+				books_2[i]->paper = select_p;
+				select_p = nullptr;
+				placed = true;
+				break;
+			}
+		}
+		if (!placed)
+		{
+			select_p->SetPosition(select_p->prevPos);
+			select_p->col->setNewPos(select_p->prevPos);
+			select_p = nullptr;
+		}
+	}
+}
+
+void Bookshelf2::RightClick(glm::vec3 screen, glm::vec3 world)
+{
+	if (!pass)
+	{
+		if (select_r == nullptr)
+		{
+			std::cout << "Check\n";
+			for (int i = 0; i < books_2.size(); i++)
+			{
+				if (books_2[i]->col->isClicked(screen.x, screen.y))
+				{
+					select_r = books_2[i];
+				}
+			}
+		}
+	}
+}
+
+void Bookshelf2::RightRelease(glm::vec3 screen, glm::vec3 world)
+{
+	if (select_r != nullptr)
+	{
+		if (select_r->col->isClicked(screen.x, screen.y) && select_r->paper != nullptr)
+		{
+			select_r->paper->SetPosition(select_r->paper->prevPos);
+			select_r->paper->col->setNewPos(select_r->paper->prevPos);
+			select_r->paper->paste = false;
+			select_r->paper->SetTexture(select_r->paper->norm_paperTexture);
+			select_r->paper->SetSize(229, -161);
+		}
+		select_r->paper = nullptr;
+	}
+	select_r = nullptr;
+}
+
+void Bookshelf2::ActionAfterPuzzle()
+{
+
+}
+
+void Bookshelf2::CompletePuzzle()
+{
+
+}
+
+Bookshelf2::~Bookshelf2()
+{
+	for (int i = 0; i < images.size(); i++)
+	{
+		delete images[i];
+	}
+	for (int j = 0; j < log.size(); j++)
+	{
+		delete log[j];
+	}
+	for (int i = 0; i < books.size(); i++)
+	{
+		delete books[i];
+	}
+	for (int i = 0; i < books_2.size(); i++)
+	{
+		delete books_2[i];
+	}
+	for (int i = 0; i < papers.size(); i++)
+	{
+		delete papers[i];
+	}
+}
+
+BookshelfPuzzle_2::BookshelfPuzzle_2()
+{
+
+}
+
+void BookshelfPuzzle_2::Render()
+{
+
+}
+
+void BookshelfPuzzle_2::Update()
+{
+
+}
+
+void BookshelfPuzzle_2::Reset()
+{
+
+}
+
+bool BookshelfPuzzle_2::CheckRequirements()
+{
+
+}
+
+void BookshelfPuzzle_2::LeftClick(glm::vec3 screen, glm::vec3 world)
+{
+
+}
+
+void BookshelfPuzzle_2::LeftRelease(glm::vec3 screen, glm::vec3 world)
+{
+
+}
+
+void BookshelfPuzzle_2::RightClick(glm::vec3 screen, glm::vec3 world)
+{
+
+}
+
+void BookshelfPuzzle_2::RightRelease(glm::vec3 screen, glm::vec3 world)
+{
+
+}
+
+void BookshelfPuzzle_2::UpdateMouseState(glm::vec3 screen, glm::vec3 world)
+{
+
+}
+
+void BookshelfPuzzle_2::CompletePuzzle()
+{
+
+}
+
+BookshelfPuzzle_2::~BookshelfPuzzle_2()
+{
+
+}
--- Enginent/Enginent/Game.cpp
+++ Enginent/Enginent/Game.cpp
@@ -50,7 +50,7 @@ void Game::Init(int width, int height)
 	loadingImage->SetSize((float)width, -(float)height);
 	loadingImage->SetTexture("Texture/tmp_texture/loading.png");
 
-	currentState = MENUSCREEN;
+	currentState = TESTSCENE;
 	cursorGame = new CursorUI();
 	UpdateScreenState();
 
